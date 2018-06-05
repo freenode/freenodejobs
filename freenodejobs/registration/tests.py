6bf1a1ff6e3991cdf3c17349497450726eb56c98
Chris Lamb <chris@chris-lamb.co.uk>
2018-06-05 22:52:21 +0100

    Factor out registration tests into an assertRegister utility (re. #18)

--- freenodejobs/registration/tests.py
+++ freenodejobs/registration/tests.py
@@ -1,9 +1,23 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from freenodejobs.utils.test import TestCase
 
+UserModel = get_user_model()
+
 
 class RegistrationTests(TestCase):
+    def assertRegister(self):
+        email = 'new@email.com'
+
+        self.assertPOST({
+            'email': 'new@email.com',
+            'password1': 'newpassword',
+            'password2': 'newpassword',
+        }, 'registration:view', login=None)
+
+        return UserModel.objects.get(email=email)
+
     def test_logged_in(self):
         response = self.assertGET(302, 'registration:view')
         self.assertRedirectsToUrl(response, settings.LOGIN_REDIRECT_URL)
@@ -12,11 +26,9 @@ class RegistrationTests(TestCase):
         self.assertGET(200, 'registration:view', login=None)
 
     def test_POST(self):
-        self.assertPOST({
-            'email': 'new@email.com',
-            'password1': 'newpassword',
-            'password2': 'newpassword',
-        }, 'registration:view', login=None)
+        user = self.assertRegister()
+
+        self.assertFalse(user.email_validated)
 
     def test_POST_duplicate_email(self):
         response = self.assertPOST({
